d9e8e608173f296b3ef717cb3ac8a21f3bb6b96f
Alok Singh <aloksingh36757@gmail.com>
2024-08-25 19:53:03 +0530

    currency Exchanger Project Start

---
+++ src/Components/Home.tsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+const Home: React.FC = () => {
+
+
+  return (
+    <div>
+      <h1 className='font-bold text-4xl'>Currency Converter</h1>
+      {/* upper case box top level div */}
+      <div className='border  h-[250px] w-[250px]'>
+
+
+      </div>
+    </div>
+  );
+};
+
+export default Home;
